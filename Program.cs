95ccb34d62c65e5cb7967f7fe5195265e539ab16
Lokesh Kumar <lokesh.kumar@cirium.com>
2025-07-14 11:03:11 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Asp.Versioning;
+using Asp.Versioning.Conventions;
+
+namespace APIVersioningDotNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+
+                //for versioning using Querystring and Header
+                options.ApiVersionReader = ApiVersionReader.Combine(
+                    new QueryStringApiVersionReader("api-version"),
+                    new HeaderApiVersionReader("x-api-version")
+                    );
+            }).AddMvc(options =>
+            {
+                options.Conventions.Add(new VersionByNamespaceConvention());
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
